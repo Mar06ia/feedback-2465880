267fa5340ac57b38bde14c9f89109bbb187a6a1b
Andres Meza <andresmezad2519@gmail.com>
2022-11-15 08:59:27 -0500

    proyecto react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+const App = () =>{
 
-function App() {
+    const bootcamps = [
+        {
+         id: 1,
+         name: 'PHP Bootcamp',
+         average_rating:8
+        },
+        {
+         id:2,
+         name: 'javascript Bootcamp',
+         average_rating: 10
+        }   
+    ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=''>
+        <h1>Listado de Bootcamps</h1>
+        <ul>
+            { bootcamps.map((bootcamp)=> (
+                <li>{ bootcamp.name}</li>
+              ))
+             }
+        </ul>  
     </div>
-  );
+ )
 }
 
-export default App;
+export default App
